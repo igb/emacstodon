1cd8691d47998a5c0335c79cb709132f378166a2
Ian Brown <igb@hccp.org>
2022-05-08 10:25:53 -0700

    this should work

--- emacstodon.el
+++ emacstodon.el
@@ -5,12 +5,11 @@
 
   (interactive)
 
-;  (if (<= (buffer-size) 500)
+  (if (<= (buffer-size) 500)
       (send-toot
-       "HELOWORLD"
-       ;(buffer-substring-no-properties (point-min) (point-max))
+       (buffer-substring-no-properties (point-min) (point-max))
        )
- ;   (print "MORE THAN 500 CHARS!"))
+    (print "MORE THAN 500 CHARS!"))
    )
 
 
@@ -18,6 +17,7 @@
   "Read auth tokens from local file"
   (if (file-exists-p "~/.emacstodon")
       (read-creds-from-emacstodon)
+    (print "Could not find credentials at ~/.emacstodon")
     )
   )
 
@@ -31,14 +31,19 @@
 	(setq cred-nvps (mapcar (lambda (x) (split-string x "=")) raw-creds))
 	(mapcar (lambda (x) (set (intern (car x)) (car (cdr x)))) cred-nvps)))
 
+
+(defun escape-uri (x)
+  (url-hexify-string x))
+
+
 (defun send-toot(toot-body)
   (read-creds)
-  (print ACCESS_TOKEN)
-  (setq status (concat "status=" toot-body))
-  (print status)
+
+  (setq status (concat "status=" (escape-uri toot-body)))
+
   (setq content-length (number-to-string(length status)))
   (setq headers (concat "Accept: */*\r\n"
-			"Host: mastodon.hccp.org\r\n" 
+			"Host: " MASTODON_HOST "\r\n" 
 			"Content-Type: application/x-www-form-urlencoded\r\n"
 			"Content-Length: "
 			content-length
@@ -46,7 +51,7 @@
 			"Authorization: Bearer "
 			ACCESS_TOKEN))
 
-  (setq conn (open-network-stream "toot" nil  "mastodon.hccp.org" 443  :type 'tls))
+  (setq conn (open-network-stream "toot" nil  MASTODON_HOST 443  :type 'tls))
   (set-process-filter conn 'keep-output)
   (setq body (concat "POST /api/v1/statuses HTTP/1.1\r\n" headers "\r\n\r\n" status))
  
@@ -54,7 +59,7 @@
 
 
 (defun keep-output (process output)
-  "Manage and process output anc check status of Tweet action."
+  "Manage and process output anc check status of Toot action."
   (sleep-for 3) ;; async is hard 
   (setq lines (split-string output "\r\n" t))
   (if (string-prefix-p "HTTP/1.1 200 OK" (car lines))
